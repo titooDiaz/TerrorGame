c1fe12a103b86606cbcb209e42e54d3fc33b0a73
Miguel Angel <miguelitodiaz169@gmail.com>
2025-06-07 18:17:54 -0500

    add butterflies 🦋

--- horror-game/Scripts/mobs.gd
+++ horror-game/Scripts/mobs.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
-@onready var animatespider = $spider/spider/AnimationPlayer
+@onready var animatespider = $spider/AnimationPlayer
+@onready var animatebutterflies = $"../Enviroment/butterflies/CollisionShape3D/butterflies/AnimationPlayer"
 
 func _ready():
 	animatespider.play("Armature|Armature|ArmatureAction")
+	animatebutterflies.play("Take 001")
+	animatebutterflies.get_animation("Take 001").loop = true
